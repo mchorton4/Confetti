62765f1555d0ebdaab6fba59af42987deb7aa87d
mchorton4 <mhorton4@ewu.edu>
2022-10-13 10:59:07 -0700

    added confetti
    Added confetti js, updated html and css

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 // My Scripts
 
 console.log('Page Loaded');
+
+//Run it
+let confetti = new Confetti("confetti");
+// let confetti2 = new Confetti('confetti2');
+
+// Optionalize it- Edit given parameters
+confetti.setCount(100);
+confetti.setSize(2);
+confetti.setPower(25);
+confetti.setFade(false);
+confetti.destroyTarget(false);
