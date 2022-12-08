930a6523c363a6eebf6c9a30756dd0135650c1bd
Malia Horton <114179957+mchorton4@users.noreply.github.com>
2022-12-07 23:21:06 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-//https://confettijs.org/confetti.min.js
+
 //https://codepen.io/Malia_Horton4/pen/qBYLawe.js
 //https://codepen.io/Malia_Horton4/pen/yLjZjKX.js
 
@@ -6,16 +6,5 @@
 
 new CircleType(document.getElementById("demo3"));
 
-//Run it
-let confetti = new Confetti("confetti");
-// let confetti2 = new Confetti('confetti2');
-
-
-// Optionalize it- Edit given parameters
-confetti.setCount(100);
-confetti.setSize(2);
-confetti.setPower(25);
-confetti.setFade(false);
-confetti.destroyTarget(false);
 
 console.log("Page Loaded");
